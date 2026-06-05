985a9bbb25267ca08a4b3699cdf644949019b911
Lucas Mendes <devsdmf@gmail.com>
2026-06-05 18:39:20 +0200

    Removed s3bucket definition on backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,13 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   storage,
 });
-
-const s3Bucket = backend.storage.resources.bucket as s3.Bucket;
-s3Bucket.autoDeleteObjects = false;
