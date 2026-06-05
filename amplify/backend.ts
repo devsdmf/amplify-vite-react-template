f631decca6381c82c801c0d7b8133ff8e4c50f0c
Lucas Mendes <devsdmf@gmail.com>
2026-06-05 18:29:25 +0200

    Fixed bucket definition

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 const backend = defineBackend({
   auth,
@@ -9,4 +10,5 @@ const backend = defineBackend({
   storage,
 });
 
-backend.storage.resources.bucket.autoDeleteObjects = false;
+const s3Bucket = backend.storage.resources.bucket as s3.Bucket;
+s3Bucket.autoDeleteObjects = false;
